5776ccbe2cca3b057d694906aef0d8ecb4d0b27d
Rafael Nowrotek <mail@benignware.com>
2016-11-14 00:13:16 +0100

    Update build

--- dist/jquery.reveal.js
+++ dist/jquery.reveal.js
@@ -11,36 +11,48 @@
   }
 }(function ($) {
 
+  function delayed(delay) {
+    var dfd = $.Deferred();
+    window.setTimeout(function() {
+      dfd.resolve();
+    }, delay);
+    return dfd.promise();
+  }
 
   // http://stackoverflow.com/questions/4774746/jquery-ajax-wait-until-all-images-are-loaded
-  function imagesLoaded(element) {
-
+  function imagesLoaded(element, options) {
+    options = options || {};
     var $element = $(element || this);
     // get all the images (excluding those with no src attribute)
     var $imgs = $element.is('img') ? $(element) : $element.find('img');
+
     // if there's no images, just return an already resolved promise
     if (!$imgs.length) {return $.Deferred().resolve().promise();}
 
     // for each image, add a deferred object to the array which resolves when the image is loaded (or if loading fails)
     var dfds = [];
-    $imgs.each(function(){
-
+    $imgs.each(function() {
         var dfd = $.Deferred();
+        var $this = $(this);
+        if (options.lazyLoad && $this.data('src')) {
+          $this.attr('src', $this.data('src'));
+        }
+        var src = $this.attr('src');
         dfds.push(dfd);
         var img = new window.Image();
         img.onload = function() {
           dfd.resolve();
         };
         img.onerror = function() {
-          dfd.resolve();
+          dfd.reject();
         };
-        img.src = this.src;
-
+        img.src = src;
     });
 
     // return a master promise object which will resolve when all the deferred objects have resolved
     // IE - when all the images are loaded
     return $.when.apply($,dfds);
+
   }
 
 
@@ -57,6 +69,7 @@
     elements = [],
     revealed = [],
     scrollTop = $(window).scrollTop(),
+    initialUpdate = false,
     queue = [],
     queueIsRunning = false,
     current = null,
@@ -67,6 +80,7 @@
         current = null;
         return;
       }
+
       queue = queue.sort(function(a, b) {
         var
           aTop = Math.floor($(a).offset().top),
@@ -82,33 +96,52 @@
         queueIsRunning = true;
       }
       var element = current;
-      imagesLoaded().then(function() {
-        $(element).css('opacity', '');
-        // Apply animation only if the element is still in view
-        if (inView(element)) {
-          var animationClass = $(element).data('animation-class') || options.animationClass;
-          var animatedClass = $(element).data('animated-class') || options.animatedClass;
-          $(element).addClass(animatedClass);
-          $(element).addClass(animationClass);
-        }
-      });
-      window.setTimeout(function() {
-        next();
-      }, delay);
+      var delayPromise = options.delay > 0 ? delayed(options.delay) : true;
+      var imagePromise = imagesLoaded(element, {lazyLoad: options.lazyLoad});
+      if (options.imagesBlock) {
+        $.when(delayPromise, imagePromise).done(function() {
+          // Apply animation only if the element is still in view
+          showElement(element);
+          // Shift next element in queue
+          next();
+        });
+      } else {
+        $.when(delayPromise, imagePromise).done(function() {
+          // Apply animation only if the element is still in view
+          showElement(element);
+        });
+        $.when(delayPromise).done(function() {
+          // Shift next element in queue
+          next();
+        });
+      }
+
     };
 
-  function update() {
 
+  function showElement(element, skip, options) {
+    var $element = $(element);
+    options = options || $element.data('reveal').getOptions();
+    var animatedClass = $element.data('animated-class') || options.animatedClass;
+    var pausedClass = $element.data('paused-class') || options.pausedClass;
+    if (skip) {
+      $element.css('animation-duration', '0s');
+    }
+    $element.removeClass(pausedClass);
+    $element.addClass(animatedClass);
+  }
+
+  function update() {
     var viewTop = $(window).scrollTop();
     var viewBottom = viewTop + $(window).innerHeight();
-
     var scrollDelta = viewTop - scrollTop;
-    scrollTop = viewTop;
-
-    if (scrollDelta === 0) {
-      return;
+    var initial = false;
+    if (scrollDelta === 0 && !initialUpdate) {
+      initialUpdate = true;
+      initial = true;
     }
 
+    scrollTop = viewTop;
     elements.forEach(function(element, index) {
       var
         options = $(element).data('reveal').getOptions(),
@@ -120,8 +153,8 @@
 
       if (top >= viewTop && top <= viewBottom || bottom >= viewTop && bottom <= viewBottom) {
         // Element is in view
-        if (revealed.indexOf(element) === -1 && (top >= viewTop && top <= viewBottom - offset || bottom >= viewTop && bottom <= viewBottom - offset)) {
-          // Element is in view - offset and it has not been revealed yet 
+        if (revealed.indexOf(element) === -1 && ( !options.initialOffset && initial || (top >= viewTop && top <= viewBottom - offset || bottom >= viewTop && bottom <= viewBottom - offset) ) ) {
+          // Element is in view - offset and it has not been revealed yet
           revealed.push(element);
           queue.push(element);
           if (!queueIsRunning) {
@@ -131,10 +164,8 @@
       } else {
         var above = viewTop > bottom && height > 0;
         if (viewTop > bottom && height > 0) {
-          // Above view, abort
-          var animationClass = $(element).data('animation-class') || options.animationClass;
-          $(element).removeClass(animationClass);
-          $(element).css('opacity', '');
+          // Above view, immediately run animation to the end.
+          showElement(element, true);
           elements.splice(index, 1);
           // Remove queued element
           var queueIndex = queue.indexOf(element);
@@ -144,8 +175,6 @@
           if (current === element) {
             next();
           }
-        } else {
-          //$(element).css('visibility', 'hidden');
         }
       }
     });
@@ -172,7 +201,10 @@
       opts = $.extend(true, {
         "delay": 400,
         "offset": 0,
-        "class": "animated"
+        "animatedClass": "animated",
+        "skipInitial": true,
+        "lazyLoad": false,
+        "initialOffset": false
       }, options),
       viewTop = $(window).scrollTop(),
       viewBottom = viewTop + $(window).innerHeight(),
@@ -181,10 +213,11 @@
       top = viewTop + bounds.top,
       bottom = top + height;
 
-    if (viewBottom < top && elements.indexOf(element) === -1) {
+    if (elements.indexOf(element) === -1 && (viewBottom < top || !opts.skipInitial)) {
       // The element is up to be revealed
-      $(element).css('opacity', '0');
       elements.push(element);
+    } else if (top < viewBottom && opts.skipInitial) {
+      showElement(element, true, opts);
     }
 
     this.getOptions = function() {
@@ -202,27 +235,29 @@
     };
 
     // Initial update
-    this.update($.extend(true, {
+    /*this.update($.extend(true, {
       delay: 0
-    }, options, $element.data()));
+    }, options, $element.data()));*/
   }
 
   // Add Plugin to registry
   $.fn.reveal = function() {
     var
-      args = [].slice.call(arguments);
-    return this.each(function() {
-      return (function(instance) {
-        var
-          result;
-        // Update or init plugin
-        $(this).data('reveal', instance = instance ? typeof args[0] === 'object' && instance.update(args[0]) && instance || instance : new Reveal(this, args[0]));
-        // Call method
-        result = typeof args[0] === 'string' && typeof instance[args[0]] === 'function' ? instance[args[0]].apply(instance, args.slice(1)) : result;
-        // Return undefined or chaining element
-        return typeof result !== 'undefined' ? result : this;
-      }).call(this, $(this).data('reveal'));
-    });
+      args = [].slice.call(arguments),
+      result = this.each(function() {
+        return (function(instance) {
+          var
+            result;
+          // Update or init plugin
+          $(this).data('reveal', instance = instance ? typeof args[0] === 'object' && instance.update(args[0]) && instance || instance : new Reveal(this, args[0]));
+          // Call method
+          result = typeof args[0] === 'string' && typeof instance[args[0]] === 'function' ? instance[args[0]].apply(instance, args.slice(1)) : result;
+          // Return undefined or chaining element
+          return typeof result !== 'undefined' ? result : this;
+        }).call(this, $(this).data('reveal'));
+      });
+    update();
+    return result;
   };
 
 }));
